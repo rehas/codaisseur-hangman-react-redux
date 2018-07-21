13cb3e4f7b69f5236d3f4e8eeee5f206cdfeecc1
RehaS <beratson@gmail.com>
2018-07-21 11:54:58 +0200

    Reducers for currentWord and userGuesses setup, combined reducers, plan-update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
-const reducer = (state = {key:[]}, action = {}) => {
-  switch (action.type) {
-  default:
-    return state
-  }
-}
+import { combineReducers } from 'redux'
+import currentWord from './currentWord'
+import userGuesses from './userGuesses'
 
-export default reducer
+// const reducer = (state = {key:[]}, action = {}) => {
+//   switch (action.type) {
+//   default:
+//     return state
+//   }
+// }
+
+export default combineReducers({
+  currentWord,
+  userGuesses
+})
